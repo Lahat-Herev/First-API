9a4e9937b406db841e4e0ebb1a6a66f0343612d3
Lahat-Herev <pedroarthuralvesdeoliveira@hotmail.com>
2022-11-04 17:43:57 -0300

    First commit

---
+++ Context/AgendaContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using ModuloAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModuloAPI.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Contato> Contatos { get; set; }
+    }
+}
